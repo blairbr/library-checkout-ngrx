70d7adae9766faf32a55ea275ba3f1b037aa3a79
Blair Brown <blair.brown@accenture.com>
2023-03-20 22:28:24 -0300

    displaying the book titles and authors

--- src/app/checked-out-books/checked-out-books.component.ts
+++ src/app/checked-out-books/checked-out-books.component.ts
@@ -10,17 +10,4 @@ import { Book } from '../book.model';
 })
 export class CheckedOutBooksComponent {
   constructor(private httpClient: HttpClient) {}
-
-  private bookURL : string = 'https://www.googleapis.com/books/v1/volumes?q=harry+potter';
-  //public bookList : Observable<Book> = of([]);
-  ngOnInit() {
-    this.getBooks().subscribe(x=>console.log("pizza", x));
-  }
-
-  getBooks() : Observable<Book[]> {
-    return this.httpClient.get<{items: Book[]}>(this.bookURL).pipe(map(x=>x.items));
-    //this.bookList.subscribe(x => console.log("x is:" , x, "type of x: ", typeof(x)));
-  }
-
-
 }
--- src/app/library-books/library-books.component.ts
+++ src/app/library-books/library-books.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map, Observable, of } from 'rxjs';
+import { Book } from '../book.model';
 
 @Component({
   selector: 'app-library-books',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class LibraryBooksComponent {
 
+  public bookList$: Observable<Book[]> = of([]);
+  private bookURL : string = 'https://www.googleapis.com/books/v1/volumes?q=harry+potter';
+  ngOnInit() {
+    this.getBooks().subscribe(x=>console.info("french fries", x));
+  }
+
+  public getBooks() : Observable<Book[]> {
+    this.bookList$ = this.httpClient.get<{items: Book[]}>(this.bookURL).pipe(map(x=>x.items));
+    return this.bookList$;
+  }
+
+  constructor(private httpClient: HttpClient) {}
 }
