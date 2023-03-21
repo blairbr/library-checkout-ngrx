4cb7bf5cd1c419a84b5bb97e9c8ed946054efe8d
blairbr <blair.j.brown@gmail.com>
2023-03-20 22:52:07 -0300

    cleaned up imports

--- src/app/checked-out-books/checked-out-books.component.ts
+++ src/app/checked-out-books/checked-out-books.component.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { map, Observable, of } from 'rxjs';
-import { Book } from '../book.model';
 
 @Component({
   selector: 'app-checked-out-books',
